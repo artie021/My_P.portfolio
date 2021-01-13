f2b99c75c95f99874b842cb3e6984cc78e8887c5
Ａｒｔｉｅ_ｃｈａｎ <63522257+artie021@users.noreply.github.com>
2021-01-13 16:55:23 -0300

    Add
    Adicionando funcionalidades

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,12 @@
             $('.navbar').removeClass('sticky')
          }
      })
+
+     //script do toggle-bar menu/navbar
+
+     $('.menu-btn').click(function(){
+         $('.navbar .menu').toggleClass('active')
+         $('.menu-btn i').toggleClass('active')
+     })
+
  }) 
